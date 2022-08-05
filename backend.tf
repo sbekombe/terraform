799ae324ec46b195ac4cb7b2db434941667fb922
sbekombe <99056370+sbekombe@users.noreply.github.com>
2022-08-05 01:17:06 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 #    backend "s3" {
 #     bucket = "code-sharing-bucket"
 #     key    = "avinash/s3file-3.tfstate"
-#     region = "us-west-2"
+#     region = "us-east-1"
 #     profile = "jespo"
 # dynamodb_table= "firstprojecttable"
 #    }
